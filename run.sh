d4d3e8ac1825836f0ec8f3b8542dccbcb6fe5696
Vipin G K <38074230+vipigk@users.noreply.github.com>
2026-02-22 14:38:42 +0000

    added-script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#python -v
+
+#Clone the repo
+
+git clone https://github.com/vipigk/fask-portfolio-main.git
+cd Flask-portfolio-main
+
+#Create & activate virtual environment
+
+python -m venv venv
+source venv/bin/activate
+
+#Install dependencies
+pip install -r requirements.txt
+
+#Run the Flask app
+flask run
